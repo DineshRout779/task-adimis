34591aa2d10f6931aa68639683fa9ff4f6fbe954
Dinesh Rout <rddinesh779@gmail.com>
2024-02-23 15:23:51 +0530

    feat: setup columns and tasks with redux-toolkit

--- client/src/features/tasks/taskSlice.ts
+++ client/src/features/tasks/taskSlice.ts
@@ -13,22 +13,48 @@ export type Task = {
   updatedAt: Date;
 };
 
-export type Tasks = {
+export type Column = {
+  id: string;
+  title: string;
   tasks: Task[];
 };
 
-const initialState: Tasks = {
+type InitialState = {
+  tasks: Task[];
+  columns: Column[];
+};
+
+const initialState: InitialState = {
   tasks: [],
+  columns: [],
 };
 
 export const taskSlice = createSlice({
   name: 'tasks',
   initialState,
-  reducers: {},
+  reducers: {
+    setTasks: (state, action) => {
+      // console.log(action.payload);
+      state.tasks = action.payload.tasks;
+    },
+    setColumns: (state, action) => {
+      state.columns = action.payload;
+    },
+    updateTask: (state, action) => {
+      console.log(action.payload);
+
+      state.tasks = state.tasks.map((task: Task) =>
+        task.id == action.payload.id
+          ? { ...task, status: action.payload.status }
+          : task
+      );
+    },
+  },
 });
 
-export const {} = taskSlice.actions;
+export const { setTasks, setColumns, updateTask } = taskSlice.actions;
 
 export const selectTasks = (state: RootState) => state.tasks.tasks;
+export const selectColumns = (state: RootState) => state.tasks.columns;
 
 export default taskSlice.reducer;
